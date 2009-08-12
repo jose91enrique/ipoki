8ee34ba163c114617fc4b7838e6ca50d62b2e640
javier.cancela.vicente <javier.cancela.vicente@23aaf443-e245-0410-abc5-852f359abe04>
2009-08-12 16:37:16 +0000

    Changes in threading. New class for updating distance and bearing in a different thread. Synchronization.

--- trunk/ipoki/Android/Ipoki/src/com/ipoki/android/ARView.java
+++ trunk/ipoki/Android/Ipoki/src/com/ipoki/android/ARView.java
@@ -1,6 +1,9 @@
 package com.ipoki.android;
 
 import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
@@ -13,6 +16,26 @@ public class ARView extends View implements SensorEventListener {
     public ARView(Context context) {
 		super(context);
 	}
+    
+    protected void onDraw(Canvas canvas) {
+    	super.onDraw(canvas);
+
+    	Paint paint = new Paint();
+        paint.setStyle(Paint.Style.FILL);
+        paint.setColor(Color.YELLOW);
+        paint.setTextSize(20);
+
+        canvas.drawText("Ipoki for Android", 160, 100, paint);
+        int y = 20;
+        int canvasHeight = canvas.getHeight();
+        for(Friend f: Ipoki.mFriends) {
+        	double d[] = f.getDistanceBearing();
+        	canvas.drawText(f.mName + " : " + Double.toString(d[0]) + " - " + Double.toString(d[1]), 20, y, paint);
+        	y += 20;
+        	if (y > canvasHeight - 20)
+        		break;
+        }
+    }
 
 	@Override
 	public void onAccuracyChanged(Sensor arg0, int arg1) {
--- trunk/ipoki/Android/Ipoki/src/com/ipoki/android/Ipoki.java
+++ trunk/ipoki/Android/Ipoki/src/com/ipoki/android/Ipoki.java
@@ -1,10 +1,24 @@
 package com.ipoki.android;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.List;
+
 import android.app.Activity;
+import android.content.Context;
 import android.hardware.Sensor;
 import android.hardware.SensorManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Window;
 import android.view.ViewGroup.LayoutParams;
 
@@ -13,7 +27,14 @@ public class Ipoki extends Activity {
     private SensorManager mSensorManager;
 	private CameraView mCameraView;
 	private ARView mARView;
-	
+	private LocationManager mLocationManager=null;
+	private LocationListener mLocationListener=null;
+	static double mLatitude;
+	static double mLongitude;
+	static final String mUserKey = "utXHANIfJeLGgDmfvEjgreRLS";
+	static Friend[] mFriends = null;
+	static FriendsUpdateThread mFriendsUpdateThread = null;
+
 	
     /** Called when the activity is first created. */
     @Override
@@ -43,6 +64,15 @@ public class Ipoki extends Activity {
         {
         	mSensorManager.registerListener(mARView, listSensors.get(0), SensorManager.SENSOR_DELAY_UI);
         }
+        
+		mLocationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+		mLocationListener = new MyLocationListener();
+		mLocationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, mLocationListener);
+		mLocationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 0, 0, mLocationListener);
+		
+		getFriends();
+		
+		mFriendsUpdateThread = new FriendsUpdateThread();
     }
     
     @Override
@@ -69,5 +99,88 @@ public class Ipoki extends Activity {
     protected void onStop()
     {
         super.onStop();
+    }
+    
+    private class MyLocationListener implements LocationListener {
+
+		@Override
+		public void onLocationChanged(Location location) {
+			if (location != null) {
+				mLatitude = location.getLatitude();
+				mLongitude = location.getLongitude();
+			}
+			
+		}
+
+		@Override
+		public void onProviderDisabled(String provider) {
+		}
+
+		@Override
+		public void onProviderEnabled(String provider) {
+		}
+
+		@Override
+		public void onStatusChanged(String provider, int status, Bundle extras) {
+		}
+    }
+    
+    private void getFriends(){
+    	URL url;
+		String userUrl = getString(R.string.friends_url) + mUserKey;
+		try {
+			url = new URL(userUrl);
+			new DownloadFriends().execute(url);
+		} catch (MalformedURLException e) {
+			e.printStackTrace();
+		} 
+    }
+
+    private class DownloadFriends extends AsyncTask<URL, Integer, Friend[]> {
+
+    	@Override
+		protected Friend[] doInBackground(URL... params) {
+			String friendsString = "";
+	    	try {
+	    		HttpURLConnection urlConnection = (HttpURLConnection)params[0].openConnection();
+	    		int responseCode = urlConnection.getResponseCode();
+
+	    		if (responseCode == HttpURLConnection.HTTP_OK) {
+	    			InputStream is = urlConnection.getInputStream();
+	    			BufferedReader br = new BufferedReader(new InputStreamReader(is));
+	    			friendsString = br.readLine();
+	    		}
+	    	} catch (IOException e) {
+				e.printStackTrace();
+			}
+	    	return processFriends(friendsString);
+		}
+    	
+    	private Friend[] processFriends(String result)
+    	{
+	    	String[] friendsData = null;
+	    	friendsData = result.substring(3).split("\\${3}");
+	    	
+	    	if (friendsData.length % 4 != 0)
+	    		Log.w("Ipoki", "Malformed data from server");
+
+	    	int friendsNum = friendsData.length / 4;
+	    	Friend[] friends = new Friend[friendsNum];
+	    	for (int i = 0; i < friendsNum; i++) {
+	    		friends[i] = new Friend(friendsData[4 * i], 
+	    								friendsData[4 * i + 1], 
+	    								friendsData[4 * i + 2], 
+	    								friendsData[4 * i + 3]);
+	    		friends[i].updateDistanceBearing(mLongitude, mLatitude);
+	    		double[] d = friends[i].getDistanceBearing();
+	    		Log.i("Ipoki", friends[i].mName + " : " + Double.toString(d[0]) + " - " + Double.toString(d[1]));
+	    	}
+	    	
+	    	return friends;
+    	}
+		
+	    protected void onPostExecute(Friend[] friends) {
+	    	mFriends = friends;
+	    }
     }
 }
