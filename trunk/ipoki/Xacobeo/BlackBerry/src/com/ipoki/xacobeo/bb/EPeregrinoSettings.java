b357d3cfdae0d5c3becc99a73b0e9e01679db890
javier.cancela.vicente <javier.cancela.vicente@23aaf443-e245-0410-abc5-852f359abe04>
2010-01-03 17:55:33 +0000

    Settings class

---
+++ trunk/ipoki/Xacobeo/BlackBerry/src/com/ipoki/xacobeo/bb/EPeregrinoSettings.java
@@ -0,0 +1,8 @@
+package com.ipoki.xacobeo.bb;
+
+public class EPeregrinoSettings {
+	public static String UserName;
+	public static String UserKey;
+	public static String Recording;
+	public static String Private;
+}
