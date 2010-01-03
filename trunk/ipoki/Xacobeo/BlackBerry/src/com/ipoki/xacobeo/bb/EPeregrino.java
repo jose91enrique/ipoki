43d2d47ee4cf00f89ccceb0e550a74a4911a6d8d
javier.cancela.vicente <javier.cancela.vicente@23aaf443-e245-0410-abc5-852f359abe04>
2010-01-03 12:12:35 +0000

    Login Screen with logo.

---
+++ trunk/ipoki/Xacobeo/BlackBerry/src/com/ipoki/xacobeo/bb/EPeregrino.java
@@ -0,0 +1,18 @@
+package com.ipoki.xacobeo.bb;
+
+import net.rim.device.api.ui.UiApplication;
+
+import com.ipoki.xacobeo.bb.screens.LoginScreen;
+
+public class EPeregrino extends UiApplication {
+	public EPeregrino() {
+		LoginScreen loginScreen = new LoginScreen();
+		pushScreen(loginScreen);
+	}
+	
+	public static void main(String[] args) {
+		EPeregrino app = new EPeregrino();
+		app.enterEventDispatcher();
+	}
+
+}
