01d17a9e39dfa1d14a0b02e3456d8e0e2bf02a98
javier.cancela.vicente <javier.cancela.vicente@23aaf443-e245-0410-abc5-852f359abe04>
2010-01-24 11:07:25 +0000

    v0.3 Base version complete. From now on, only debugging.

--- trunk/ipoki/Xacobeo/BlackBerry/src/com/ipoki/xacoveo/bb/screens/AboutScreen.java
+++ trunk/ipoki/Xacobeo/BlackBerry/src/com/ipoki/xacoveo/bb/screens/AboutScreen.java
@@ -1,7 +1,16 @@
 package com.ipoki.xacoveo.bb.screens;
 
+import net.rim.device.api.system.ApplicationDescriptor;
+import net.rim.device.api.system.Bitmap;
+import net.rim.device.api.system.Display;
+import net.rim.device.api.ui.Font;
+import net.rim.device.api.ui.FontFamily;
+import net.rim.device.api.ui.Graphics;
+import net.rim.device.api.ui.Ui;
 import net.rim.device.api.ui.component.LabelField;
+import net.rim.device.api.ui.container.HorizontalFieldManager;
 import net.rim.device.api.ui.container.MainScreen;
+import net.rim.device.api.ui.container.VerticalFieldManager;
 
 import com.ipoki.xacoveo.bb.XacoVeoSettings;
 import com.ipoki.xacoveo.bb.local.XacoveoLocalResource;
@@ -9,6 +18,60 @@ import com.ipoki.xacoveo.bb.local.XacoveoLocalResource;
 public class AboutScreen extends MainScreen implements XacoveoLocalResource {
 	
 	public AboutScreen() {
-		add(new LabelField(XacoVeoSettings.XacoveoResource.getString(ABOUT_SCR_ABOUT)));
+		super(NO_VERTICAL_SCROLL);
+		Bitmap tmpLogo = Bitmap.getBitmapResource("res/logo_b.png");
+		final int displayWidth = Display.getWidth();
+		final int displayHeight = Display.getHeight();
+		int spaceWidth = 180;
+		int topSpacerHeight = 85;
+		Font appFont;
+		try {
+			FontFamily fontFam = FontFamily.forName("BBAlpha Sans");
+			if (displayWidth < 320) {
+				tmpLogo = Bitmap.getBitmapResource("res/logo_s.png");
+				appFont = fontFam.getFont(Font.PLAIN, 8, Ui.UNITS_pt);
+				spaceWidth = 100;
+				topSpacerHeight = 50;
+			}
+			else if (displayWidth < 370) {
+				tmpLogo = Bitmap.getBitmapResource("res/logo_m.png");
+				appFont = fontFam.getFont(Font.PLAIN, 9, Ui.UNITS_pt);
+				spaceWidth = 120;
+				topSpacerHeight = 60;
+			}
+			else {
+				appFont = fontFam.getFont(Font.PLAIN, 9, Ui.UNITS_pt);
+			}
+			Font.setDefaultFont(appFont);
+		} catch (ClassNotFoundException e) {
+		}
+		final Bitmap logoBitmap = tmpLogo;
+		
+		
+		VerticalFieldManager mainManager = new VerticalFieldManager(
+				VerticalFieldManager.USE_ALL_WIDTH | VerticalFieldManager.USE_ALL_HEIGHT | VerticalFieldManager.NO_VERTICAL_SCROLL){
+		    public void paint(Graphics graphics) {
+		        graphics.drawBitmap(0, 0, displayWidth, displayHeight, logoBitmap, 0, 0);
+		        super.paint(graphics);
+		    }            
+		};
+		mainManager.add(new SpacerField(displayWidth, topSpacerHeight));
+		
+		VerticalFieldManager spaceManager = new VerticalFieldManager(VerticalFieldManager.NO_VERTICAL_SCROLL);
+		SpacerField sfLeft = new SpacerField(spaceWidth, 20);
+		spaceManager.add(sfLeft);
+		VerticalFieldManager vertManager = new VerticalFieldManager();
+		vertManager.add(new LabelField(XacoVeoSettings.XacoveoResource.getString(ABOUT_SCR_XACOVEO_BB)));
+		vertManager.add(new LabelField(XacoVeoSettings.XacoveoResource.getString(ABOUT_SCR_PEXEGO)));
+		vertManager.add(new LabelField(XacoVeoSettings.XacoveoResource.getString(ABOUT_SCR_VERSION) + 
+				ApplicationDescriptor.currentApplicationDescriptor().getVersion()));
+		vertManager.add(new LabelField(XacoVeoSettings.XacoveoResource.getString(ABOUT_SCR_URL)));
+		
+		HorizontalFieldManager horManager = new HorizontalFieldManager();
+		horManager.add(spaceManager);
+		horManager.add(vertManager);
+		
+		mainManager.add(horManager);
+		add(mainManager);
 	}
 }
--- trunk/ipoki/Xacobeo/BlackBerry/src/com/ipoki/xacoveo/bb/screens/ConfigurationScreen.java
+++ trunk/ipoki/Xacobeo/BlackBerry/src/com/ipoki/xacoveo/bb/screens/ConfigurationScreen.java
@@ -1,12 +1,19 @@
 package com.ipoki.xacoveo.bb.screens;
 
+import net.rim.device.api.system.Bitmap;
+import net.rim.device.api.system.Display;
 import net.rim.device.api.system.PersistentObject;
 import net.rim.device.api.system.PersistentStore;
+import net.rim.device.api.ui.Font;
+import net.rim.device.api.ui.Graphics;
 import net.rim.device.api.ui.MenuItem;
 import net.rim.device.api.ui.component.EditField;
+import net.rim.device.api.ui.component.LabelField;
 import net.rim.device.api.ui.component.Menu;
 import net.rim.device.api.ui.component.PasswordEditField;
+import net.rim.device.api.ui.component.SeparatorField;
 import net.rim.device.api.ui.container.MainScreen;
+import net.rim.device.api.ui.container.VerticalFieldManager;
 
 import com.ipoki.xacoveo.bb.XacoVeoSettings;
 import com.ipoki.xacoveo.bb.local.XacoveoLocalResource;
@@ -17,13 +24,41 @@ public class ConfigurationScreen extends MainScreen implements XacoveoLocalResou
 	EditField freqField;
 
 	public ConfigurationScreen() {
-		usernameField = new EditField(XacoVeoSettings.XacoveoResource.getString(CONF_SCR_USERNAME), XacoVeoSettings.UserName);
-		passwordField = new PasswordEditField(XacoVeoSettings.XacoveoResource.getString(CONF_SCR_PASSWORD), XacoVeoSettings.UserPassword);
-		freqField = new EditField(XacoVeoSettings.XacoveoResource.getString(CONF_SCR_UPDATE), XacoVeoSettings.UpdateFreq, 5, EditField.FILTER_NUMERIC);
+		super(NO_VERTICAL_SCROLL);
+		final Bitmap logoBitmap = Bitmap.getBitmapResource("res/fondo.png");
+		final int displayWidth = Display.getWidth();
+		final int displayHeight = Display.getHeight();
+
+		VerticalFieldManager mainManager = new VerticalFieldManager(
+				VerticalFieldManager.USE_ALL_WIDTH | VerticalFieldManager.USE_ALL_HEIGHT | VerticalFieldManager.NO_VERTICAL_SCROLL){
+		    // override pain method to create the background image
+		    public void paint(Graphics graphics) {
+		        // draw the background image
+		        graphics.drawBitmap(0, 0, displayWidth, displayHeight, logoBitmap, 0, 0);
+		        super.paint(graphics);
+		    }            
+		};
+
+		LabelField nameLabel = new LabelField(XacoVeoSettings.XacoveoResource.getString(CONF_SCR_USERNAME));
+		nameLabel.setFont(getFont().derive(Font.BOLD));
+		usernameField = new EditField("", XacoVeoSettings.UserName);
+		LabelField passLabel = new LabelField(XacoVeoSettings.XacoveoResource.getString(CONF_SCR_PASSWORD));
+		passLabel.setFont(getFont().derive(Font.BOLD));
+		passwordField = new PasswordEditField("", XacoVeoSettings.UserPassword);
+		LabelField freqLabel = new LabelField(XacoVeoSettings.XacoveoResource.getString(CONF_SCR_UPDATE));
+		freqLabel.setFont(getFont().derive(Font.BOLD));
+		freqField = new EditField("", XacoVeoSettings.UpdateFreq, 5, EditField.FILTER_NUMERIC);
 		
-		add(usernameField);
-		add(passwordField);
-		add(freqField);
+		mainManager.add(nameLabel);
+		mainManager.add(usernameField);
+		mainManager.add(new SeparatorField());
+		mainManager.add(passLabel);
+		mainManager.add(passwordField);
+		mainManager.add(new SeparatorField());
+		mainManager.add(freqLabel);
+		mainManager.add(freqField);
+		
+		add(mainManager);
 	}
 	
 	protected void makeMenu(Menu menu, int instance) {
