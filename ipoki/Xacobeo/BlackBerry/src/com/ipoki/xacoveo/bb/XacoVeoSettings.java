34404ff239b0714e2a2da4684623762b3de83eac
javier.cancela.vicente <javier.cancela.vicente@23aaf443-e245-0410-abc5-852f359abe04>
2010-08-23 09:57:26 +0000

    Cambio de iconos Cambio de url (a xacobeo.es/xacoveo)

--- ipoki/Xacobeo/BlackBerry/src/com/ipoki/xacoveo/bb/XacoVeoSettings.java
+++ ipoki/Xacobeo/BlackBerry/src/com/ipoki/xacoveo/bb/XacoVeoSettings.java
@@ -7,65 +7,65 @@ import net.rim.device.api.i18n.ResourceBundle;
 import com.ipoki.xacoveo.bb.local.XacoveoLocalResource;
 
 public class XacoVeoSettings implements XacoveoLocalResource{
-	public static final long KEY = 0xe57fdf6ef36bdfe1L;
-	public static ResourceBundle XacoveoResource = ResourceBundle.getBundle(BUNDLE_ID, BUNDLE_NAME);
+        public static final long KEY = 0xe57fdf6ef36bdfe1L;
+        public static ResourceBundle XacoveoResource = ResourceBundle.getBundle(BUNDLE_ID, BUNDLE_NAME);
 
-	public static String UserName = "";
-	public static String UserPassword = "";
-	public static String UpdateFreq = "15";
-	public static String UserKey;
-	public static String Recording;
-	public static String Private;
-	public static boolean Connected = false;
-	public static long Lapse;
-	public static int Language = 0;
-	public static String ServerUrl = "http://www.xacoveo.com/";
-	
-	public static String getPositionUrl(String lat, String lon) {
-		return ServerUrl + "geoserver/ear.php?iduser=" + UserKey +
-		"&lat=" + lat + "&lon=" + lon;
-	}
+        public static String UserName = "";
+        public static String UserPassword = "";
+        public static String UpdateFreq = "15";
+        public static String UserKey;
+        public static String Recording;
+        public static String Private;
+        public static boolean Connected = false;
+        public static long Lapse;
+        public static int Language = 0;
+        public static String ServerUrl = "http://www.xacobeo.es/xacoveo/";
+        
+        public static String getPositionUrl(String lat, String lon) {
+                return ServerUrl + "geoserver/ear.php?iduser=" + UserKey +
+                "&lat=" + lat + "&lon=" + lon;
+        }
 
-	public static String getFriendsUrl() {
-		return ServerUrl + "myfriends2.php?iduser=" + UserKey;
-	}
-	
-	public static String getLoginUrl(String user, String password) {
-		return ServerUrl + "signin.php?user=" + user + "&pass=" + password;
-	}
-	
-	public static String getRecordOnUrl() {
-		return ServerUrl + "set_h_on.php?iduser=" + UserKey;
-	}
-	
-	public static String getRecordOffUrl() {
-		return ServerUrl + "set_h_off.php?iduser=" + UserKey;
-	}
-	
-	public static String getPrivateOffUrl() {
-		return ServerUrl + "set_p_off.php?iduser=" + UserKey;
-	}
+        public static String getFriendsUrl() {
+                return ServerUrl + "myfriends2.php?iduser=" + UserKey;
+        }
+        
+        public static String getLoginUrl(String user, String password) {
+                return ServerUrl + "signin.php?user=" + user + "&pass=" + password;
+        }
+        
+        public static String getRecordOnUrl() {
+                return ServerUrl + "set_h_on.php?iduser=" + UserKey;
+        }
+        
+        public static String getRecordOffUrl() {
+                return ServerUrl + "set_h_off.php?iduser=" + UserKey;
+        }
+        
+        public static String getPrivateOffUrl() {
+                return ServerUrl + "set_p_off.php?iduser=" + UserKey;
+        }
 
-	public static String getPrivateOnUrl() {
-		return ServerUrl + "set_p_on.php?iduser=" + UserKey;
-	}
+        public static String getPrivateOnUrl() {
+                return ServerUrl + "set_p_on.php?iduser=" + UserKey;
+        }
 
-	public static void setSettings(Hashtable settings) {
-		XacoVeoSettings.UserName = (String) settings.get("username");
-		XacoVeoSettings.UserPassword = (String) settings.get("password");
-		XacoVeoSettings.UpdateFreq = (String) settings.get("updatefreq");
-		String lang = (String)settings.get("language");
-		if (lang != null)
-			XacoVeoSettings.Language = Integer.parseInt(lang);
-	}
-	
-	public static Hashtable getSettings() {
-		Hashtable settings = new Hashtable();
-		settings.put("username", XacoVeoSettings.UserName);
-		settings.put("password", XacoVeoSettings.UserPassword);
-		settings.put("updatefreq", XacoVeoSettings.UpdateFreq);
-		settings.put("language", String.valueOf(XacoVeoSettings.Language));
-		
-		return settings;
-	}
+        public static void setSettings(Hashtable settings) {
+                XacoVeoSettings.UserName = (String) settings.get("username");
+                XacoVeoSettings.UserPassword = (String) settings.get("password");
+                XacoVeoSettings.UpdateFreq = (String) settings.get("updatefreq");
+                String lang = (String)settings.get("language");
+                if (lang != null)
+                        XacoVeoSettings.Language = Integer.parseInt(lang);
+        }
+        
+        public static Hashtable getSettings() {
+                Hashtable settings = new Hashtable();
+                settings.put("username", XacoVeoSettings.UserName);
+                settings.put("password", XacoVeoSettings.UserPassword);
+                settings.put("updatefreq", XacoVeoSettings.UpdateFreq);
+                settings.put("language", String.valueOf(XacoVeoSettings.Language));
+                
+                return settings;
+        }
 }
--- ipoki/Xacobeo/BlackBerry/src/com/ipoki/xacoveo/bb/screens/AboutScreen.java
+++ ipoki/Xacobeo/BlackBerry/src/com/ipoki/xacoveo/bb/screens/AboutScreen.java
@@ -16,62 +16,62 @@ import com.ipoki.xacoveo.bb.XacoVeoSettings;
 import com.ipoki.xacoveo.bb.local.XacoveoLocalResource;
 
 public class AboutScreen extends MainScreen implements XacoveoLocalResource {
-	
-	public AboutScreen() {
-		super(NO_VERTICAL_SCROLL);
-		Bitmap tmpLogo = Bitmap.getBitmapResource("res/logo_b.png");
-		final int displayWidth = Display.getWidth();
-		final int displayHeight = Display.getHeight();
-		int spaceWidth = 180;
-		int topSpacerHeight = 85;
-		Font appFont;
-		try {
-			FontFamily fontFam = FontFamily.forName("BBAlpha Sans");
-			if (displayWidth < 320) {
-				tmpLogo = Bitmap.getBitmapResource("res/logo_s.png");
-				appFont = fontFam.getFont(Font.PLAIN, 8, Ui.UNITS_pt);
-				spaceWidth = 100;
-				topSpacerHeight = 50;
-			}
-			else if (displayWidth < 370) {
-				tmpLogo = Bitmap.getBitmapResource("res/logo_m.png");
-				appFont = fontFam.getFont(Font.PLAIN, 9, Ui.UNITS_pt);
-				spaceWidth = 120;
-				topSpacerHeight = 60;
-			}
-			else {
-				appFont = fontFam.getFont(Font.PLAIN, 9, Ui.UNITS_pt);
-			}
-			Font.setDefaultFont(appFont);
-		} catch (ClassNotFoundException e) {
-		}
-		final Bitmap logoBitmap = tmpLogo;
-		
-		
-		VerticalFieldManager mainManager = new VerticalFieldManager(
-				VerticalFieldManager.USE_ALL_WIDTH | VerticalFieldManager.USE_ALL_HEIGHT | VerticalFieldManager.NO_VERTICAL_SCROLL){
-		    public void paint(Graphics graphics) {
-		        graphics.drawBitmap(0, 0, displayWidth, displayHeight, logoBitmap, 0, 0);
-		        super.paint(graphics);
-		    }            
-		};
-		mainManager.add(new SpacerField(displayWidth, topSpacerHeight));
-		
-		VerticalFieldManager spaceManager = new VerticalFieldManager(VerticalFieldManager.NO_VERTICAL_SCROLL);
-		SpacerField sfLeft = new SpacerField(spaceWidth, 20);
-		spaceManager.add(sfLeft);
-		VerticalFieldManager vertManager = new VerticalFieldManager();
-		vertManager.add(new LabelField(XacoVeoSettings.XacoveoResource.getString(ABOUT_SCR_XACOVEO_BB)));
-		vertManager.add(new LabelField(XacoVeoSettings.XacoveoResource.getString(ABOUT_SCR_PEXEGO)));
-		vertManager.add(new LabelField(XacoVeoSettings.XacoveoResource.getString(ABOUT_SCR_VERSION) + 
-				ApplicationDescriptor.currentApplicationDescriptor().getVersion()));
-		vertManager.add(new LabelField(XacoVeoSettings.XacoveoResource.getString(ABOUT_SCR_URL)));
-		
-		HorizontalFieldManager horManager = new HorizontalFieldManager();
-		horManager.add(spaceManager);
-		horManager.add(vertManager);
-		
-		mainManager.add(horManager);
-		add(mainManager);
-	}
+        
+        public AboutScreen() {
+                super(NO_VERTICAL_SCROLL);
+                Bitmap tmpLogo = Bitmap.getBitmapResource("logo_b.png");
+                final int displayWidth = Display.getWidth();
+                final int displayHeight = Display.getHeight();
+                int spaceWidth = 180;
+                int topSpacerHeight = 85;
+                Font appFont;
+                try {
+                        FontFamily fontFam = FontFamily.forName("BBAlpha Sans");
+                        if (displayWidth < 320) {
+                                tmpLogo = Bitmap.getBitmapResource("logo_s.png");
+                                appFont = fontFam.getFont(Font.PLAIN, 8, Ui.UNITS_pt);
+                                spaceWidth = 100;
+                                topSpacerHeight = 50;
+                        }
+                        else if (displayWidth < 370) {
+                                tmpLogo = Bitmap.getBitmapResource("logo_m.png");
+                                appFont = fontFam.getFont(Font.PLAIN, 9, Ui.UNITS_pt);
+                                spaceWidth = 120;
+                                topSpacerHeight = 60;
+                        }
+                        else {
+                                appFont = fontFam.getFont(Font.PLAIN, 9, Ui.UNITS_pt);
+                        }
+                        Font.setDefaultFont(appFont);
+                } catch (ClassNotFoundException e) {
+                }
+                final Bitmap logoBitmap = tmpLogo;
+                
+                
+                VerticalFieldManager mainManager = new VerticalFieldManager(
+                                VerticalFieldManager.USE_ALL_WIDTH | VerticalFieldManager.USE_ALL_HEIGHT | VerticalFieldManager.NO_VERTICAL_SCROLL){
+                    public void paint(Graphics graphics) {
+                        graphics.drawBitmap(0, 0, displayWidth, displayHeight, logoBitmap, 0, 0);
+                        super.paint(graphics);
+                    }            
+                };
+                mainManager.add(new SpacerField(displayWidth, topSpacerHeight));
+                
+                VerticalFieldManager spaceManager = new VerticalFieldManager(VerticalFieldManager.NO_VERTICAL_SCROLL);
+                SpacerField sfLeft = new SpacerField(spaceWidth, 20);
+                spaceManager.add(sfLeft);
+                VerticalFieldManager vertManager = new VerticalFieldManager();
+                vertManager.add(new LabelField(XacoVeoSettings.XacoveoResource.getString(ABOUT_SCR_XACOVEO_BB)));
+                vertManager.add(new LabelField(XacoVeoSettings.XacoveoResource.getString(ABOUT_SCR_PEXEGO)));
+                vertManager.add(new LabelField(XacoVeoSettings.XacoveoResource.getString(ABOUT_SCR_VERSION) + 
+                                ApplicationDescriptor.currentApplicationDescriptor().getVersion()));
+                vertManager.add(new LabelField(XacoVeoSettings.XacoveoResource.getString(ABOUT_SCR_URL)));
+                
+                HorizontalFieldManager horManager = new HorizontalFieldManager();
+                horManager.add(spaceManager);
+                horManager.add(vertManager);
+                
+                mainManager.add(horManager);
+                add(mainManager);
+        }
 }
